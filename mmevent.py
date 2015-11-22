28c5f31db654a29e99dd5a53ebdf5a921da1f42e
littlepretty <littlepretty881203@gmail.com>
2015-11-22 15:41:33 -0600

    More comments
    Signed-off-by: littlepretty <littlepretty881203@gmail.com>

--- mmevent.py
+++ mmevent.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 
 class MMEvent(object):
-    """Abstraction of packet arrival or departure
-
-    Attributes:
-        pkt_id: which packet this event is about
-        event_type(str): 'arrival' or 'departure'
-        time_stamp: when should we handle this event;
-            we need this value to put event into event_list
-        enter_time: the moment it enter the system
-        exit_time: the moment it exit the system
-    """
+    """Abstraction of packet arrival or departure"""
     def __init__(self, pkt_id, name, ts):
+        """Create MMEvent object
+
+        Attributes:
+            pkt_id: which packet this event is about
+            event_type(str): 'arrival' or 'departure'
+            time_stamp: when should we handle this event;
+                we need this value to put event into event_list
+            enter_time: the moment it enter the system
+            exit_time: the moment it exit the system
+        """
         super(MMEvent, self).__init__()
         self.pkt_id = pkt_id
         self.evt_name = name
