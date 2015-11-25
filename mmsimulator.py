01d084ce1bacac005659b266c83ca552f8defb9b
littlepretty <littlepretty881203@gmail.com>
2015-11-24 22:52:06 -0600

    Driver; Dump the time and num_pkt_inside sequence
    Driver in simulator
    Rename a couple of counters
    Add log_time and log_num_pkt_inside list in system states
            Interpretation: from this @time point on, the number of packet
            in the system is @num_pkt_inside.
    
    Signed-off-by: littlepretty <littlepretty881203@gmail.com>

--- mmsimulator.py
+++ mmsimulator.py
@@ -3,17 +3,18 @@
 from mmevent import MMEvent
 from random_generator import MMGenerate
 from mmsystem import MMSystem
+from mmreporter import MMReporter
 import time
 import numpy as np
 
 class MMSimulator(object):
     """Simulator for M/M/2/7 system"""
-    def __init__(self, system, end_time):
+    def __init__(self, system, obsrv_int, end_time):
         super(MMSimulator, self).__init__()
         self.system = system
         self.init_event_list()
+        self.obsrv_int = obsrv_int
         self.end_time = end_time
-        self.pkt_seen = 0
         self.clock = 0
 
     def init_event_list(self):
@@ -58,11 +59,12 @@ class MMSimulator(object):
         N = len(arrive_time_seq)
         while self.system.pkt_served + self.system.pkt_dropped < N and self.should_continue():
             # schedule/add a new pkt arrive event
-            if self.pkt_seen < N:
-                new_arrival_ts = arrive_time_seq[self.pkt_seen]
-                new_arrive = MMEvent(self.pkt_seen, 'arrival', new_arrival_ts)
+            if self.system.pkt_seen < N:
+                new_arrival_ts = arrive_time_seq[self.system.pkt_seen]
+                new_arrive = MMEvent(self.system.pkt_seen, \
+                                     'arrival', new_arrival_ts)
                 self.event_list.append(new_arrive)
-                self.pkt_seen += 1
+                self.system.pkt_seen += 1
 
             # pop up the next event
             evt_x = self.next_event()
@@ -73,14 +75,19 @@ class MMSimulator(object):
                 # set the serving server to 'idle'
                 # increase @pkt_served counter
                 # calculate how long this pkt spend in @system
-                if self.system.waiting == 0:
+                if self.system.pkt_waiting == 0:
                     self.system.srv_status[evt_x.depart_srv] = 'idle'
                 else:
-                    self.system.waiting -= 1
-                self.system.pkt_served += 1
+                    self.system.pkt_waiting -= 1
+
                 evt_x.exit_time = self.clock
-                spend = evt_x.exit_time - evt_x.enter_time
-                self.system.spending_time[evt_x.pkt_id] = spend
+                # either server became idle or waiting pkt decreased
+                # log num_pkt_inside the system
+                self.system.dump_num_pkt_inside(self.clock)
+                # calculate the spending time of this packet
+                self.system.dump_pkt_spending_time(evt_x)
+                self.system.pkt_served += 1
+
             if evt_x.evt_name == 'arrival':
                 if self.system.full():
                     # just drop pkt and increase counter
@@ -90,42 +97,59 @@ class MMSimulator(object):
                         # put pkt into one available server
                         # calculate when it should exit the server
                         # mark this server as 'busy'
-                        new_depart_ts = self.clock + depart_time_seq[evt_x.pkt_id]
+                        new_depart_ts = self.clock + \
+                                        depart_time_seq[evt_x.pkt_id]
                         new_depart_srv = self.system.available_server()
                         self.system.srv_status[new_depart_srv] = 'busy'
                     else:
                         # find the server pkt should go
                         earliest_ts, earliest_srv = self.schedule_departure()
-                        new_depart_ts = earliest_ts + depart_time_seq[evt_x.pkt_id]
+                        new_depart_ts = earliest_ts + \
+                                        depart_time_seq[evt_x.pkt_id]
                         new_depart_srv = earliest_srv
-                        self.system.waiting += 1
+                        self.system.pkt_waiting += 1
 
-                    new_depart = MMEvent(evt_x.pkt_id, 'departure', new_depart_ts)
+                    # either server became busy or waiting packet increases
+                    # log num_pkt_inside the system
+                    self.system.dump_num_pkt_inside(self.clock)
+                    # actually insert new departure event
+                    new_depart = MMEvent(evt_x.pkt_id, \
+                                         'departure', new_depart_ts)
                     new_depart.enter_time = self.clock
                     new_depart.depart_srv = new_depart_srv
-
                     self.event_list.append(new_depart)
 
-        print [round(x, 2) for x in self.system.spending_time.values()]
+def simulator_driver():
+    num_srv = 2
+    num_buffer = 5
+    num_pkt_inside = 0
+    end_time = 1000
+    pkt_seq_len = 10
 
+    mm27 = MMSystem(num_srv, num_buffer, num_pkt_inside)
 
-def main():
-    ns = 2
-    nb = 5
-    et = 100000
-    mm27 = MMSystem(ns, nb)
-    simulator = MMSimulator(mm27, et)
-    ats = MMGenerate(10, 0.5, int(time.time()))
-    dts = MMGenerate(10, 1, int(time.time()))
-    ats = np.cumsum(ats)
+    ats = MMGenerate(pkt_seq_len, 0.5, int(time.time())) # arrival interval
+    ats = np.cumsum(ats) # arrival time stamp
+
+    dts = MMGenerate(pkt_seq_len, 1, int(time.time())) # departure time stamp
+
     ats = [round(i, 2) for i in ats]
     dts = [round(j, 2) for j in dts]
-    #print ats
-    #print dts
-    #time.sleep(10)
-    ats = [6.09, 6.65, 7.66, 8.41, 9.0]
-    dts = [7.32, 1.13, 2.0, 1.51, 1.17]
+
+    observe_interval = 0.05
+    # ots = [observe_interval for _ in range(0, \
+            # int(end_time / observe_interval) + 1)]
+    # ots = np.cumsum(ots) # obverse time stamp
+
+    simulator = MMSimulator(mm27, observe_interval, end_time)
     simulator.simulate_core(ats, dts)
+    reporter = MMReporter(mm27)
+    print reporter.blocking_prob()
+    print reporter.mean_time_spending_in_system()
+    print reporter.mean_num_pkt_in_system()
+
+def main():
+    simulator_driver()
 
 if __name__ == '__main__':
     main()
--- mmsystem.py
+++ mmsystem.py
@@ -1,33 +1,61 @@
 #!/usr/bin/python
 
 class MMSystem(object):
-    """Various counters for M/M/2/2+5 system
+    """Various system states and counters for M/M/2/2+5 system
 
     Attributes:
         dropped_pkt: number of packet dropped so far
-        waiting: number of packet in the system queue
+        pkt_waiting: number of packet in the system queue
         pkt_served: number of packet exited
         spending_time: how much each packet is spending in the system
     """
-    def __init__(self, num_srv, capacity):
+    def __init__(self, num_srv, capacity, num_pkt_inside):
         super(MMSystem, self).__init__()
-        self.pkt_dropped = 0
-        self.waiting = 0
-        self.pkt_served = 0
-        self.spending_time = {}
+        # immutable properties
         self.num_srv = num_srv
         self.capacity = capacity
+
+        # couters
+        self.pkt_dropped = 0
+        self.pkt_seen = 0
+        self.pkt_waiting = 0
+        self.pkt_served = 0
+
+        # result log info
+        self.spending_time = {}
+        self.log_time = []
+        self.log_time.append(0)
+        self.log_num_pkt_inside = []
+        self.log_num_pkt_inside.append(num_pkt_inside)
+
+        # state variables
+        self.stable = False
         self.srv_status = {}
         for i in range(0, self.num_srv):
             self.srv_status[i] = 'idle'
 
     def full(self):
         """Test if the queue is full"""
-        return self.waiting >= self.capacity
+        return self.pkt_waiting >= self.capacity
 
     def available(self):
         """Test if any server is idle"""
         return 'idle' in self.srv_status.values()
+
+    def num_available_servers(self):
+        counter = 0
+        for key, value in self.srv_status.items():
+            if value == 'idle':
+                counter += 1
+        return counter
+
+    def dump_num_pkt_inside(self, time):
+        self.log_time.append(time)
+        num_pkt_inside = self.pkt_waiting + self.num_available_servers()
+        self.log_num_pkt_inside.append(num_pkt_inside)
+
+    def dump_pkt_spending_time(self, evt):
+        self.spending_time[evt.pkt_id] = evt.exit_time - evt.enter_time
 
     def available_server(self):
         """Find any 'idle' server in system"""
