99c225712f86d393349a2ad85e4796211e13ebf9
littlepretty <littlepretty881203@gmail.com>
2015-11-24 22:55:23 -0600

    Reporter class helps calculations in problem 3
    Blocking probability
    Mean time spending in system
    Mean number of packet in the system
    
    Signed-off-by: littlepretty <littlepretty881203@gmail.com>

---
+++ mmreporter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+class MMReporter(object):
+    def __init__(self, system):
+        super(MMReporter, self).__init__()
+        self.system = system
+
+    def blocking_prob(self):
+        return float(self.system.pkt_dropped) / float(self.system.pkt_seen)
+
+    def mean_time_spending_in_system(self):
+        dur_sum = 0.0
+        n = float(len(self.system.spending_time.values()))
+        for dur in self.system.spending_time.values():
+            dur_sum += dur
+        return dur_sum / n
+
+    def mean_num_pkt_in_system(self):
+        num_pkt_duration = {}
+        entire_duration = 0.0
+
+        for i in range(0, len(self.system.log_time) - 1):
+            dur = self.system.log_time[i+1] - self.system.log_time[i]
+            num_pkt = self.system.log_num_pkt_inside[i]
+            if num_pkt in num_pkt_duration:
+                num_pkt_duration[num_pkt] += dur
+            else:
+                num_pkt_duration[num_pkt] = dur
+
+            entire_duration += dur
+
+        product_sum = 0.0
+        for num_pkt, dur in num_pkt_duration.items():
+            product_sum += num_pkt * dur
+        return product_sum / entire_duration
+
+
