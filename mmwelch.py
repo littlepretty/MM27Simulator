e01f3ae88bbad53a1cbadfe995eb12d2265be8a0
littlepretty <littlepretty881203@gmail.com>
2015-11-27 21:16:18 -0600

    A simple Welch module to average multiple runs
    1. A large number of runs is needed to get smooth averaged run
    2. Put report stuff to doc/
    3. Should try average-moving-window to eliminate high-frequency error?
    
    Signed-off-by: littlepretty <littlepretty881203@gmail.com>

---
+++ mmwelch.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import numpy as np
+from matplotlib import pyplot as plt
+import math
+
+class MMWelch(object):
+    """Welch graphic method to eliminate warm-up period"""
+    def __init__(self, num_replicas, interval, run_length):
+        super(MMWelch, self).__init__()
+        self.num_replicas = num_replicas
+        self.run_length = run_length
+        self.interval = interval
+        self.avg_run = [0] * self.run_length
+        self.time_seq = [self.interval * i for i in range(0, self.run_length)]
+
+    def average_all_runs(self):
+        for i in range(0, self.num_replicas):
+            # truncate only the common part of each runs
+            replica = np.loadtxt('%d.txt' % i, dtype=int)
+            print "run length = ", len(replica)
+            replica = replica[:self.run_length]
+            self.avg_run = np.add(replica, self.avg_run)
+        # average and save
+        self.avg_run = [float(x) / float(self.num_replicas) for x in self.avg_run]
+        np.savetxt('avg_run.txt', self.avg_run)
+
+    def plot_avg_run(self):
+        y_max = math.ceil(max(self.avg_run))
+
+        plt.figure()
+        plt.plot(self.time_seq, self.avg_run)
+        plt.xticks(np.arange(0, self.time_seq[-1], 10))
+        plt.yticks(np.arange(0, y_max, 0.2))
+        plt.grid()
+        plt.savefig('avg_%d_runs.eps' % self.num_replicas, format='eps')
+
